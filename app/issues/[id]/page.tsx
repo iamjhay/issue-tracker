961b2275df40143c8287bc242844fa5e44a6d752
John Edet <jhaydrille75@gmail.com>
2023-10-19 15:19:55 +0100

    Add the ediit button

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,8 +1,10 @@
 import { IssueStatusBadge } from "@/app/component";
 import prisma from "@/prisma/client";
-import { Box, Card, Flex, Heading } from "@radix-ui/themes";
+import { Box, Button, Card, Flex, Grid, Heading } from "@radix-ui/themes";
+import Link from "next/link";
 import { notFound } from "next/navigation";
 import Markdown from "react-markdown";
+import { Pencil2Icon } from "@radix-ui/react-icons";
 
 interface Props {
   params: { id: string };
@@ -15,22 +17,29 @@ const IssueDetailPage = async ({ params }: Props) => {
       id: parseInt(params.id),
     },
   });
-
   // display 404 page if issue is not found!
   if (!issue) notFound();
 
   //   await delay(2000);
   return (
-    <Box>
-      <Heading>{issue?.title}</Heading>
-      <Flex gap={"4"} my={"4"}>
-        <IssueStatusBadge status={issue?.status} />
-        <p>{issue?.createdAt.toDateString()}</p>
-      </Flex>
-      <Card className="prose" mt={"4"}>
-        <Markdown>{issue?.description}</Markdown>
-      </Card>
-    </Box>
+    <Grid columns={{ initial: "1", md: "2" }} gap={"4"}>
+      <Box>
+        <Heading>{issue?.title}</Heading>
+        <Flex gap={"4"} my={"4"}>
+          <IssueStatusBadge status={issue?.status} />
+          <p>{issue?.createdAt.toDateString()}</p>
+        </Flex>
+        <Card className="prose" mt={"4"}>
+          <Markdown>{issue?.description}</Markdown>
+        </Card>
+      </Box>
+      <Box>
+        <Button>
+          <Pencil2Icon />
+          <Link href={`issues/${issue.id}/edit`}>Edit Issue</Link>
+        </Button>
+      </Box>
+    </Grid>
   );
 };
 
