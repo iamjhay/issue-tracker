ea04c78fda31c45d8e1bad792d0af349d9b0b169
John <jhaydrille75@gmail.com>
2023-10-27 17:59:08 +0100

    Sort Issues

--- app/issues/list/IssuesToolbar.tsx
+++ app/issues/list/IssuesToolbar.tsx
@@ -1,11 +1,12 @@
 import { Button, Flex } from "@radix-ui/themes";
 import Link from "next/link";
 import IssueStatusFilter from "./issueStatusFilter";
+import { SearchParamsProps } from "./page";
 
-const IssuesToolbar = () => {
+const IssuesToolbar = ({ searchParams }: SearchParamsProps) => {
   return (
     <Flex justify={"between"} className="mb-5">
-      <IssueStatusFilter />
+      <IssueStatusFilter searchParams={searchParams} />
       <Button variant="soft">
         <Link href="/issues/new">New Issues</Link>
       </Button>
--- app/issues/list/issueStatusFilter.tsx
+++ app/issues/list/issueStatusFilter.tsx
@@ -2,8 +2,11 @@
 import React from "react";
 import { Select } from "@radix-ui/themes";
 import { Status } from "@prisma/client";
+import { useRouter } from "next/navigation";
+import { SearchParamsProps } from "./page";
 
-const IssueStatusFilter = () => {
+const IssueStatusFilter = ({ searchParams }: SearchParamsProps) => {
+  const router = useRouter();
   const statuses: { label: string; value?: Status }[] = [
     { label: "All" },
     { label: "Open", value: "OPEN" },
@@ -11,12 +14,26 @@ const IssueStatusFilter = () => {
     { label: "Closed", value: "CLOSED" },
   ];
   return (
-    <Select.Root>
+    <Select.Root
+      onValueChange={(status) => {
+        const query = status !== " " ? { ...searchParams, status } : "";
+        const searchParam = new URLSearchParams(query);
+        const queryString = searchParam.toString();
+        if (query === "") {
+          router.push("/issues/list/");
+        } else {
+          router.push("/issues/list?" + queryString);
+        }
+      }}
+    >
       <Select.Trigger placeholder="Filter by Status..." />
       <Select.Content>
         <Select.Group>
           {statuses.map((status) => (
-            <Select.Item key={status.label} value={status.value || " "}>
+            <Select.Item
+              key={status.label}
+              value={status.value || undefined || " "}
+            >
               {status.label}
             </Select.Item>
           ))}
