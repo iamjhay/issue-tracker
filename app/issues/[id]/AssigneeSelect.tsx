478505acd1c09c5a12a7caccbd621e52eb5c41c9
John <jhaydrille75@gmail.com>
2023-10-27 12:19:45 +0100

    Show Toast notifications

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -6,6 +6,7 @@ import { Issue, User } from "@prisma/client";
 import axios from "axios";
 import { useQuery } from "@tanstack/react-query";
 import { Skeleton } from "@/app/component";
+import toast, { Toaster } from "react-hot-toast";
 
 const AssigneeSelect = ({ issue }: { issue: Issue }) => {
   const {
@@ -24,35 +25,46 @@ const AssigneeSelect = ({ issue }: { issue: Issue }) => {
   if (isLoading) return <Skeleton />;
 
   return (
-    <Select.Root
-      defaultValue={issue.assignedToUserId || ""}
-      onValueChange={(userId) => {
-        if (userId === "null") {
-          axios.patch("/api/issues/" + issue.id, {
-            assignedToUserId: null,
-          });
-        } else {
-          axios.patch("/api/issues/" + issue.id, {
-            assignedToUserId: userId,
-          });
-        }
-      }}
-    >
-      <Select.Trigger placeholder="Assign…" />
-      <Select.Content>
-        <Select.Group>
-          <Select.Label className="px-[25px] text-xs leading-[25px] text-mauve11">
-            Suggestions
-          </Select.Label>
-          <Select.Item value="null">Unassigned</Select.Item>
-          {users?.map((user) => (
-            <Select.Item key={user.id} value={user.id}>
-              {user.name}
-            </Select.Item>
-          ))}
-        </Select.Group>
-      </Select.Content>
-    </Select.Root>
+    <>
+      <Select.Root
+        defaultValue={issue.assignedToUserId || ""}
+        onValueChange={(userId) => {
+          if (userId === "null") {
+            axios
+              .patch("/api/issues/" + issue.id, {
+                assignedToUserId: null,
+              })
+              .catch(() => {
+                toast.error("Changes could not be saved");
+              });
+          } else {
+            axios
+              .patch("/api/issues/" + issue.id, {
+                assignedToUserId: userId,
+              })
+              .catch(() => {
+                toast.error("Changes could not be saved");
+              });
+          }
+        }}
+      >
+        <Select.Trigger placeholder="Assign…" />
+        <Select.Content>
+          <Select.Group>
+            <Select.Label className="px-[25px] text-xs leading-[25px] text-mauve11">
+              Suggestions
+            </Select.Label>
+            <Select.Item value="null">Unassigned</Select.Item>
+            {users?.map((user) => (
+              <Select.Item key={user.id} value={user.id}>
+                {user.name}
+              </Select.Item>
+            ))}
+          </Select.Group>
+        </Select.Content>
+      </Select.Root>
+      <Toaster />
+    </>
   );
 };
 
